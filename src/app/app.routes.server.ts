01857c8a2c2e1633f4c9925073399e497e36ecb0
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-11-23 20:01:44 -0500

    refactor

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,22 @@
+import { inject } from '@angular/core';
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { ProductService } from './domains/products/product.service';
+import { firstValueFrom, map } from 'rxjs';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'view/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const productService = inject(ProductService);
+      return await firstValueFrom(
+        productService.products$.pipe(
+          map(products => products.map(product => product.id)),
+          map(ids => ids.map(id => ({ id: String(id) }))),
+        ),
+      );
+    },
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender,
